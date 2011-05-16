2e72cdfa9faab689d2ea6bf3a3b95e002d547c9e
Alex B <persuadotron@gmail.com>
2011-05-16 17:40:23 +0800

    Added functionality for multiple methods to be checked.

--- Checks/src/checks/GuardCheck.java
+++ Checks/src/checks/GuardCheck.java
@@ -11,7 +11,7 @@ import com.puppycrawl.tools.checkstyle.api.*;
  */
 public class GuardCheck extends Check
 {
-	private String mName;
+	private String[] mName;
 
 	/**
 	 * Return integer array of unique Token Types to visit
@@ -23,15 +23,15 @@ public class GuardCheck extends Check
 	}
 	
 	/**
-	 * Visit each token identified by the default token type
+	 * Visit each Method Def token
 	 * 
-	 * For each method def AST
-	 * - check if the method is one specified to be checked by the config xml (methodName property)
-	 * - slist -> literal if (expr) -> { slist (expr/assignment) }
+	 * At each Method Def token:
+	 * 1. Compare the method name with the list of methods to be checked (Set through the XML configuration file)
+	 * 2. If it is a match, check the method has applied the Guard Pattern 
 	 * 
 	 * TODO
 	 * - ensure that no further assignments are made to vars outside of the if statement
-	 * - ensure there is no 'else' component to the if statement
+	 * - add check for the if condition (should fail on if(true) etc)
 	 */
 	@Override
 	public void visitToken(DetailAST ast)
@@ -42,62 +42,63 @@ public class GuardCheck extends Check
 		String methodName = s.substring(0, s.indexOf('['));
 		
 		
-		// If the method name is the specified one to check - check it
-		if (this.mName.equals(methodName))
+		// If the method name is the specified to be check - check it
+		for(String m: this.mName)
 		{
-			System.out.println("Visiting method: " +methodName);
-			
-			// Check that the method contains the guard pattern
-			// First we look for a statement list
-			DetailAST list = ast.findFirstToken(TokenTypes.SLIST);
-			
-			if (list != null)
+			if (m.equals(methodName))
 			{
+				System.out.println("Visiting method: " +methodName);
+				
+				// Check that the method contains the guard pattern
+				// First we look for a statement list
+				DetailAST list = ast.findFirstToken(TokenTypes.SLIST);
+				
 				/* Now need to check that the statement list complies with the guard pattern
 				 *  - check if statement present
 				 *  - check no else statement
 				 *  - check variable used in the check is not used elsewhere in the method (?)
 				 */
-				if (!list.branchContains(TokenTypes.LITERAL_IF))
+				if (list != null)
 				{
-					System.out.println("\tExpected if statement NOT found.\tFAIL");
-					log(list.getLineNo(), "Guard pattern not present in '" +methodName+ "' method.");
-				}
-				else
-				{
-					// Check no else component
-					if (!list.findFirstToken(TokenTypes.LITERAL_IF).branchContains(TokenTypes.LITERAL_ELSE))
+					if (!list.branchContains(TokenTypes.LITERAL_IF))
 					{
-						System.out.println("\tGuard pattern found.\t\t\tPASS");
+						System.out.println("\tExpected if statement NOT found.\tFAIL");
+						log(list.getLineNo(), "Guard pattern not present in '" +methodName+ "' method.");
 					}
 					else
 					{
-						// Else found which should not be present in guard pattern
-						log(list.getLineNo(), "Guard pattern incorrectly implemented in ''" +methodName+ "''. Else statement present.");
+						// Check no else component
+						if (!list.findFirstToken(TokenTypes.LITERAL_IF).branchContains(TokenTypes.LITERAL_ELSE))
+						{
+							System.out.println("\tGuard pattern found.\t\t\tPASS");
+						}
+						else
+						{
+							// Else found which should not be present in guard pattern
+							log(list.getLineNo(), "Guard pattern incorrectly implemented in ''" +methodName+ "''. Else statement present.");
+						}
 					}
 				}
+				else
+				{
+					System.out.println("\tNo statement list found - method is strange =S.");
+				}		
 			}
-			else
-			{
-				System.out.println("\tNo statement list found - method is strange.");
-			}		
-		}
-		else
-		{
-			// TODO report not the method we are looking for (?) 
 		}
 	}
 	
 	/**
 	 * Sets the method name property
-	 * @param mName the name of the method in the source Java file to perform the Guard pattern check
+	 * @param mName string array of methods to perform the Guard Pattern check for
 	 */
-	public void setMethodName(String mName)
+	public void setMethodName(String[] mName)
 	{
-		// Need to extend this idea to be able to have multiple methods
-		// given as this string then manipulate into a list/array of methods to check
-		System.out.println("Method to be checked: " +mName);
 		this.mName = mName;
+		// Verbose message to be removed later
+		for(String s: this.mName)
+		{
+			System.out.println("Method to be checked: " +s);
+		}
 	}
 
 }
